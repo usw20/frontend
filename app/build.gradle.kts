4f4b4e9ad67f938e87bd1c38b997b9e86916f3e4
Park1234 <psj04100410262635@gmail.com>
2025-08-18 18:37:34 +0900

    두번쨰 커밋

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.airbnb.android:lottie:6.4.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
